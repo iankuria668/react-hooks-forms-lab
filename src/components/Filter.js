d356cf302172fd7e0b0404de1c5fd1ef43453322
iankuria668 <iankuria668@gmail.com>
2024-05-08 08:35:58 +0300

    FIlter and ItemForm Components created

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchChange }) {
+  const [filterItem, setFilterItem] = useState("All");
+  const [searchText, setSearchText] = useState("");
+  
+  const handleCategoryChange = (event) => {
+    const category = event.target.value;
+    setFilterItem(category);
+    onCategoryChange(category);
+  };
+    
+  const handleSearchChange = (event) => {
+    const searchText = event.target.value;
+    setSearchText(searchText);
+    onSearchChange(searchText);
+  };
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input 
+        type="text" 
+        name="search" 
+        placeholder="Search..." 
+        onChange={handleSearchChange}
+        value={searchText}
+      />
+      <select 
+        name="filter" 
+        onChange={handleCategoryChange} 
+        value={filterItem}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
